7a478945a27232948d3bce346452d2aa79c4e7d4
hchockarprasad <hchockarprasad@gmail.com>
2019-10-17 17:40:58 +0530

    First commit

---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+console.log('Site running...');
+
+
+$(document).ready(function (){
+  console.log('Document ready');
+})
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss/,
+        loaders: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.(svg|gif|png|jpg|eot|woff|ttf)$/,
+        loaders: [
+          'url-loader'
+        ]
+      },
+      {
+        test: /\,js$/,
+        loader: 'babel-loader?presets[]=es2015'
+      },
+      {
+        test: /\.(svg|gif|png|jpg)$/,
+        use: {
+          loader: "file-loader"
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'src/index.html',
+      chunk: ['main']
+    }),
+    new CopyWebpackPlugin([
+      {from:'src/img',to:'img'} 
+    ]), 
+  ],
+}
